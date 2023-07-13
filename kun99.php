1a46429afed47e0cbcd8af693d0fe868d0827b53
Kun0319 <asd1234567896138@gmail.com>
2023-07-13 15:04:31 +0800

    7/13 99

--- kun99.php
+++ kun99.php
@@ -1,25 +1,43 @@
-<table width="100%" border="1">
+<table width="100%" hight="100%" border="1">
 
     <?php
     // 常數命名
-    define('ROWS', 2);
-    define('COL', 6);
-    define('START', 1);
+    define('ROWS', 3);
+    define('COL', 3);
+    define('START', 2);
 
 
     for ($k = 0; $k < ROWS; $k++) {
         echo '<tr>';
         for ($j = START; $j <= (START + COL - 1); $j++) {
             $newj = $j + $k * COL;
-            echo '<td>';
-            for ($i = 1; $i <= 9; $i++) {
-                $v = $newj * $i;
-                echo "{$newj}x{$i} ={$v} <br/>";
+            $p = '<td bgcolor=pink>';
+            $y = '<td bgcolor=yellow>';
+            for ($i = ROWS; $i <= (START + COL - 1); $i++) {
+                if ($j % 2 == 0 && $k % 2 == 0) {
+                    echo $y;
+                } else if ($j % 2 != 0 && $k % 2 == 0) {
+                    echo $p;
+                } else if ($j % 2 != 0 && $k % 2 == 1) {
+                    echo $y;
+                } else if ($j % 2 == 0 && $k % 2 == 1) {
+                    echo $p;
+                } else {
+                    echo 'Hello,ERROR';
+                }
+
+                for ($i = 1; $i <= 9; $i++) {
+                    $v = $newj * $i;
+                    echo "{$newj}x{$i} ={$v} <br/>";
+                }
+                echo '</td>';
             }
-            echo '</td>';
         }
-        echo '</tr>';
     }
+
+
+    echo '</tr>';
+
 
     ?>
 
