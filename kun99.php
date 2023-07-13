7fdfc8e54f0d0bfbd785d500041f196067f3950c
Kun0319 <asd1234567896138@gmail.com>
2023-07-13 11:53:29 +0800

    7/13 99

--- kun99.php
+++ kun99.php
@@ -1,10 +1,16 @@
 <table width="100%" border="1">
 
     <?php
-    for ($k = 0; $k < 5; $k++) {
+    // 常數命名
+    define('ROWS', 2);
+    define('COL', 6);
+    define('START', 1);
+
+
+    for ($k = 0; $k < ROWS; $k++) {
         echo '<tr>';
-        for ($j = 2; $j <= 5; $j++) {
-            $newj = $j + $k * 4;
+        for ($j = START; $j <= (START + COL - 1); $j++) {
+            $newj = $j + $k * COL;
             echo '<td>';
             for ($i = 1; $i <= 9; $i++) {
                 $v = $newj * $i;
