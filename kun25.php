2acde98afc414fb55fd2f28deedf4e38036521d4
Kun0319 <asd1234567896138@gmail.com>
2023-07-17 16:30:04 +0800

    7/17

---
+++ kun25.php
@@ -0,0 +1,6 @@
+<h1> Kun big Company</h1>
+<hr />
+kun: <img src="kun24.php?rate=76" alt=""><br>
+Tony: <img src="kun24.php?rate=25" alt=""><br>
+Perter: <img src="kun24.php?rate=63" alt=""><br>
+Amy: <img src="kun24.php?rate=55" alt=""><br>
